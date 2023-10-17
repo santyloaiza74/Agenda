73f7c1aafad62574f5dadc08fbc987f870c138ee
santyloaiza74 <123980290+santyloaiza74@users.noreply.github.com>
2023-10-17 00:04:19 -0500

    Delete Backend directory

--- Backend/src/database/models/index.js
+++
@@ -1,8 +0,0 @@
-const { User,UserSchema } = require('./user.model')
-
-function setupModels(sequelize) {
-    User.init(UserSchema, User.config(sequelize))
-    User.associate(sequelize.models)
-}
-
-module.exports = setupModels
