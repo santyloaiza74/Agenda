8b1bd63209fd5e4f38d5c71ae8fc50f76b513166
santyloaiza74 <santyloaiza74@gmail.com>
2023-10-17 00:22:10 -0500

    Consultas completadas

---
+++ Backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const { port: APP_PORT, port } = require('./config/config')
+const routerApi = require('./routes')
+
+const app = express()
+
+app.use(express.json())
+
+routerApi(app)
+
+app.listen(port, () => {
+    console.log(`APP corriendo por el puerto ${port}`
+    )
+})
